3d1b904bc71ffa933cea07af96139090cbe6741a
Lucas Rezende <rezendelc94@gmail.com>
2024-05-28 09:46:19 -0300

    feat: added product routes

---
+++ src/infrastructure/api/__tests__/product.e2e.spec.ts
@@ -0,0 +1,62 @@
+import { app, sequelize } from "../express";
+import request from "supertest";
+
+describe("E2E test for product", () => {
+  beforeEach(async () => {
+    await sequelize.sync({ force: true });
+  });
+
+  afterAll(async () => {
+    await sequelize.close();
+  });
+
+  it("should create a product", async () => {
+    const response = await request(app)
+      .post("/product")
+      .send({
+        name: "Product 1",
+        price: 1
+      });
+
+    expect(response.status).toBe(200);
+    expect(response.body.id).toBeDefined()
+    expect(response.body.name).toBe("Product 1");
+    expect(response.body.price).toBe(1)
+
+  });
+
+  it("should not create a product", async () => {
+    const response = await request(app).post("/product").send({
+      name: "Product 1",
+    });
+    expect(response.status).toBe(500);
+  });
+
+  it("should list all product", async () => {
+    const response = await request(app)
+      .post("/product")
+      .send({
+        name: "Product 1",
+        price: 1
+      });
+    expect(response.status).toBe(200);
+    const response2 = await request(app)
+      .post("/product")
+      .send({
+        name: "Product 2",
+        price: 5
+      });
+    expect(response2.status).toBe(200);
+
+    const listResponse = await request(app).get("/product").send();
+
+    expect(listResponse.status).toBe(200);
+    expect(listResponse.body.products.length).toBe(2);
+    const product = listResponse.body.products[0];
+    expect(product.name).toBe("Product 1");
+    expect(product.price).toBe(1);
+    const product2 = listResponse.body.products[1];
+    expect(product2.name).toBe("Product 2");
+    expect(product2.price).toBe(5);
+  });
+});
